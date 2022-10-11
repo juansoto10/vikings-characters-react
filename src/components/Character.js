986cecb66b197442303a2143995c5902874c098b
juansoto10 <jpsoto121@gmail.com>
2022-10-11 17:43:36 -0500

    'character' component created

---
+++ src/components/Character.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Character() {
+  return (
+    <div className='character'>
+      <img 
+        className='character__img'
+        src={require('../images/char_ragnar.png')}
+        alt='Ragnar' />
+      <div className='character__txt'>
+        <h2 className='character__name'>Ragnar Lothbrok</h2>
+        <h3 className='character__features'>Viking Warrior, farmer</h3>
+        <p className='character__description'>Ragnar "Lothbrok" Sigurdsson (Old Norse: Ragnarr Loðbrók, Ragnar means "keeper of the fort" and Lothbrok means "shaggy breeches") is a Viking farmer and warrior who yearns to raid the rumored riches of undiscovered England. His first wife is Lagertha, and they have two children named Gyda and Bjorn. His second wife is Aslaug, and they have four sons named Ubbe, Hvitserk, Sigurd, and Ivar. Ragnar is the younger brother of Rollo. He becomes King of Kattegat, in southern Norway</p>
+      </div>
+    </div>
+  );
+}
+
+export default Character;

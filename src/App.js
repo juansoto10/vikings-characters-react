8cc97ae027fd88d27eeb7ecd6de305963c4814e7
juansoto10 <jpsoto121@gmail.com>
2022-10-11 23:53:44 -0500

    Character.css created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Character from './components/Character';
 function App() {
   return (
     <div className='App'>
-      <Character />
+      <main className='main-container'>
+        <h1>Vikings Characters</h1>
+        <Character />
+      </main>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../stylesheets/Character.css';
 
 function Character() {
   return (
